a1d6f5f084d40c5f297ba7e09788aaeaec869e22
minsang <110008434+jms8793@users.noreply.github.com>
2024-02-24 17:20:01 +0900

    feat : MemberServiceTest 수정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -34,5 +34,4 @@ public class MemberService {
         return memberRepository.findByID(memberId);
     }
 
-
 }
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,6 +1,8 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,12 @@ import static org.assertj.core.api.Assertions.*;
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {
@@ -38,9 +46,9 @@ class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-        }
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
         //then
 
