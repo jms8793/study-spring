4f344b3ee5b2a5730a0482fc80b91537a977a743
minsang <110008434+jms8793@users.noreply.github.com>
2024-02-24 17:03:13 +0900

    feat : add MemberServiceTest

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,62 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+@Nested
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        }
+
+
+        //then
+
+
+    }
+
+    @Test
+    void findMembers() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findOne() {
+    }
+}
